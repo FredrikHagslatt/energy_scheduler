d79f9ca3c00903aa5e416436f26070f3f2660923
Fredrik Hagslätt <fredrik.hagslatt@gmail.com>
2022-12-12 21:14:17 +0100

    Added manual mode, refarcored a bunch

--- custom_components/energy_scheduler/sensor.py
+++ custom_components/energy_scheduler/sensor.py
@@ -45,15 +45,40 @@ class EnergyScheduler(SensorEntity):
         self.raw_today = []
 
     def read_config(self, config):
-        hours_off = config.get("hours_off", 6)
-        scheduler_mode = config.get('scheduler_mode', 'default')
+        scheduler = config.get('scheduler', 'auto')
 
-        scheduler_modes = {'default', 'extreme'}
-        if not scheduler_mode in scheduler_modes:
-            scheduler_mode = 'default'
+        schedulers = {'auto', 'manual', 'always_on', 'always_off'}
+        if not scheduler in schedulers:
+            logger.error('Invalid scheduler' + str(scheduler) +
+                         '. Setting scheduler to auto')
+            scheduler = 'auto'
 
-        conf_dict = {'hours_off': hours_off,
-                     'scheduler_mode': scheduler_mode}
+        default_mode = config.get("default_mode", "on")
+
+        hours_off = config.get("hours_off", None)
+        if hours_off != None:
+            hours_off = [int(hour) for hour in hours_off.split(',')]
+
+        hours_on = config.get('hours_on', None)
+        if hours_on != None:
+            hours_on = [int(hour) for hour in hours_on.split(',')]
+
+        if scheduler == 'manual':
+            if default_mode == 'on' and hours_off == None:
+                logger.warning(
+                    'default_mode = on, no hours_off configured. Setting scheduler to always_on')
+                scheduler = 'always_on'
+            if default_mode == 'off' and hours_on == None:
+                logger.warning(
+                    'default_mode = off, no hours_on configured. Setting scheduler to always_off')
+                scheduler = 'always_off'
+
+        conf_dict = {
+            'scheduler': scheduler,
+            'default_mode': default_mode,
+            'hours_off': hours_off,
+            'hours_on': hours_on,
+        }
 
         return conf_dict
 
@@ -71,48 +96,66 @@ class EnergyScheduler(SensorEntity):
 
     def rank_hours_after_price(self, today):
         today_with_hours = [(i, today[i]) for i in range(len(today))]
-        sorted_today_with_hours = sorted(today_with_hours, key=lambda tup: tup[1], reverse=True)
+        sorted_today_with_hours = sorted(
+            today_with_hours, key=lambda tup: tup[1], reverse=True)
 
         ranked_hours = [0] * 24
         for i in range(len(sorted_today_with_hours)):
             ranked_hours[sorted_today_with_hours[i][0]] = i
         return ranked_hours
 
-    def calculate_default_mode(self, ranked_hours):
+    def calculate_auto_mode(self, ranked_hours):
+        DEFAULT_HOURS = 6
         today = []
         for rank in ranked_hours:
-            if self.config["hours_off"] > rank:
+            if DEFAULT_HOURS > rank:
                 mode = 0
             else:
                 mode = 1
             today.append(mode)
         return today
 
-    def calculate_extreme_mode(self, ranked_hours):
-        today = []
+    def calculate_manual_mode(self):
+        if self.config.get('default_mode') == 'on':
+            today = [1] * 24
+
+            for hour in self.config.get('hours_off'):
+                today[hour] = 0
+
+        if self.config.get('default_mode') == 'off':
+            today = [0] * 24
+
+            for hour in self.config.get('hours_on'):
+                today[hour] = 1
+
         return today
 
     def update(self) -> None:
-        logger.info("Updating sensor")
+        logger.info('Updating sensor')
 
         nordpool_raw_today = self.get_nordpool_raw_today()
         nordpool_today = [hour['value'] for hour in nordpool_raw_today]
-
         ranked_hours = self.rank_hours_after_price(nordpool_today)
 
-        if self.config['scheduler_mode'] == 'default':
-            self.today = self.calculate_default_mode(ranked_hours)
-        elif self.config['scheduler_mode'] == 'extreme':
-            self.today = self.calculate_extreme_mode(ranked_hours)
+        if self.config.get('scheduler') == 'auto':
+            self.today = self.calculate_auto_mode(ranked_hours)
+
+        elif self.config.get('scheduler') == 'manual':
+            self.today = self.calculate_manual_mode()
+
+        elif self.config.get('scheduler') == 'always_on':
+            self.today = [1] * 24
+
+        elif self.config.get('scheduler') == 'always_off':
+            self.today = [0] * 24
 
         this_hour = int(datetime.now().strftime("%H"))
-        if self.config["hours_off"] > ranked_hours[this_hour]:
-            self.set_mode('Off')
-        else:
+        if self.today[this_hour]:
             self.set_mode('On')
+        else:
+            self.set_mode('Off')
 
         raw_today = nordpool_raw_today
-
         for i in range(len(raw_today)):
             raw_today[i]['value'] = self.today[i]
         self.raw_today = raw_today
