69a0b2bbd3598ac08753b9e588be9a0ef3d28935
Fredrik Hagslätt <fredrik.hagslatt@gmail.com>
2023-02-15 16:16:29 +0100

    bugfix

--- custom_components/energy_scheduler/misc_entities.py
+++ custom_components/energy_scheduler/misc_entities.py
@@ -19,24 +19,22 @@ class EnergyDailyInit(SensorEntity):
         logger.info("Energy Daily Init")
         self.hass = hass
         self.last_update_date = datetime(1970, 1, 1).date()
-        self._attr_native_unit_of_measurement = ENERGY_KILO_WATT_HOUR
-        self._attr_device_class = SensorDeviceClass.ENERGY
-        self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_native_value = 0
 
     def get_energy(self):
         entity_id = 'sensor.develco_zhemi101_summation_delivered'
-        energy = self.hass.states.get(entity_id).state
+        entity = self.hass.states.get(entity_id)
+        if entity:
+            energy = entity.state
+        else:
+            energy = 0
+        logger.info(energy)
         return energy
-
-    @ property
-    def update_value(self):
-        self._attr_native_value = self.get_energy()
 
     def update(self) -> None:
         logger.info('Updating sensor')
         today = datetime.now().date()
         if self.last_update_date != today:
-            self.update_value()
+            self._attr_native_value = self.get_energy()
             self.last_update_date = today
 
